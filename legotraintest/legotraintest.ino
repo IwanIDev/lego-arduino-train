3aa920bf09ba1222b72e98674443228f492aa80e
IwanIDev <iwan@iwani.dev>
2025-07-03 15:47:12 +0100

    Added speed controls via buttons on pins 2 and 4

--- legotraintest/legotraintest.ino
+++ legotraintest/legotraintest.ino
@@ -1,45 +1,96 @@
-/*
-* Lego Train Motor Test
-* Iwan I
-* 2025-07-03
-*/
+/**
+ * Basic train motor controls
+ * Iwan I
+ * 2025-07-03
+ */
 
-#include <Lpf2Hub.h>
+#include "Lpf2Hub.h"
 
-Lpf2Hub train;
-byte motorPort = (byte) PoweredUpHubPort::B;
-char[] trainName = "testTrain";
+// create a hub instance
+Lpf2Hub trainHub;
+byte port = (byte)PoweredUpHubPort::B;
 
-int BAUD = 115200;
+enum SPEED {
+  FAST,
+  SLOW,
+  STOPPED
+};
+
+const int fastButton = 2;
+const int slowButton = 4;
+
+SPEED trainState = FAST;
 
 void setup() {
-  Serial.begin(BAUD);
-  train.init();
+    Serial.begin(115200);
+
+    pinMode(fastButton, INPUT_PULLUP);
+    pinMode(slowButton, INPUT_PULLUP);
 }
 
-bool connectTrain() {
-  train.connectHub();
+void setState() {
+  int fastButtonState = digitalRead(fastButton);
+  int slowButtonState = digitalRead(slowButton);
 
-  if (!train.isConnected()) {
-    Serial.printf("Failed to connect to train %s", trainName);
-    return false;
+  if (fastButtonState == LOW) {  // button pressed
+    trainState = FAST;
+    Serial.println("FAST selected");
+  } else if (slowButtonState == LOW) {  // button pressed
+    trainState = SLOW;
+    Serial.println("SLOW selected");
   }
-  
-  char[] address = train.getHubAddress().toString().c_str();
-  Serial.printf("Connected to train %s on address %s", trainName, address);
-  
-  return true;
+
 }
 
+int getSpeed(SPEED state) {
+  switch(state) {
+    case SLOW:
+      return 35;
+    case FAST:
+      return 50;
+    case STOPPED:
+      return 0;
+  }
+}
+
+// main loop
 void loop() {
-  if (train.isConnecting()) {
-    connectTrain();
+
+  if (!trainHub.isConnected() && !trainHub.isConnecting()) {
+    trainHub.init(); // initalize the PoweredUpHub instance
+    //trainHub.init("90:84:2b:03:19:7f"); //example of initializing an hub with a specific address
   }
 
-  if (!train.isConnected()) {
-    Serial.printf("Failed to connect to train %s", trainName);
-    return;
+  // connect flow. Search for BLE services and try to connect if the uuid of the hub is found
+  if (trainHub.isConnecting()) {
+    trainHub.connectHub();
+    if (trainHub.isConnected()) {
+      Serial.println("Connected to HUB");
+      Serial.print("Hub address: ");
+      Serial.println(trainHub.getHubAddress().toString().c_str());
+      Serial.print("Hub name: ");
+      Serial.println(trainHub.getHubName().c_str());
+    } else {
+      Serial.println("Failed to connect to HUB");
+    }
   }
-  
-  train.setBasicMotorSpeed(port, 255);
-}
+
+  setState();
+
+  // if connected, you can set the name of the hub, the led color and shut it down
+  if (trainHub.isConnected()) {
+
+    char hubName[] = "trainHub";
+    trainHub.setHubName(hubName);
+
+
+    int speed = getSpeed(trainState);
+
+    trainHub.setBasicMotorSpeed(port, speed);
+    
+  } else {
+    Serial.println("Train hub is disconnected");
+    delay(1000);
+  }
+
+} // End of loop

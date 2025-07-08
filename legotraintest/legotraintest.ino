7984862ec9ca9cada2eac1737076f2f75222d0f1
IwanIDev <iwan@iwani.dev>
2025-07-08 14:19:46 +0100

    Got the train control working with speed controls

--- legotraintest/legotraintest.ino
+++ legotraintest/legotraintest.ino
@@ -19,7 +19,10 @@ enum SPEED {
 const int fastButton = 2;
 const int slowButton = 4;
 
-SPEED trainState = FAST;
+SPEED trainState = STOPPED;
+
+unsigned long previousMillis = 0;
+const long speedSwitchInterval = 250;
 
 void setup() {
     Serial.begin(115200);
@@ -28,34 +31,46 @@ void setup() {
     pinMode(slowButton, INPUT_PULLUP);
 }
 
+/*
+* Calculates the current state based on button press.
+*/
 void setState() {
   int fastButtonState = digitalRead(fastButton);
   int slowButtonState = digitalRead(slowButton);
 
-  if (fastButtonState == LOW) {  // button pressed
+  if (fastButtonState == LOW && slowButtonState == LOW) {
+    trainState = STOPPED;
+    Serial.println("STOPPED selected");
+    delay(100);
+  } else if (fastButtonState == LOW) {  // button pressed
     trainState = FAST;
     Serial.println("FAST selected");
   } else if (slowButtonState == LOW) {  // button pressed
     trainState = SLOW;
     Serial.println("SLOW selected");
   }
-
 }
 
+/*
+* Determines the absolute speed values for the motors given
+* the state.
+*/
 int getSpeed(SPEED state) {
   switch(state) {
     case SLOW:
-      return 35;
+      return 15;
     case FAST:
-      return 50;
+      return 30;
     case STOPPED:
       return 0;
   }
 }
 
-// main loop
-void loop() {
-
+/*
+* Establish a connection to the train via BLE.
+* Returns true if successful, false otherwise.
+*/
+bool connect() {
   if (!trainHub.isConnected() && !trainHub.isConnecting()) {
     trainHub.init(); // initalize the PoweredUpHub instance
     //trainHub.init("90:84:2b:03:19:7f"); //example of initializing an hub with a specific address
@@ -72,25 +87,66 @@ void loop() {
       Serial.println(trainHub.getHubName().c_str());
     } else {
       Serial.println("Failed to connect to HUB");
+      return false;
     }
   }
+  return true;
+}
 
-  setState();
+void handleInput() {
+  const char STOP_COMMAND[] = "stop";
+  const int STOP_COMMAND_LENGTH = sizeof(STOP_COMMAND) - 1;
+  const char SLOW_COMMAND[] = "slow";
+  const int SLOW_COMMAND_LENGTH = sizeof(STOP_COMMAND) - 1;
+  const char FAST_COMMAND[] = "fast";
+  const int FAST_COMMAND_LENGTH = sizeof(STOP_COMMAND) - 1;
 
-  // if connected, you can set the name of the hub, the led color and shut it down
-  if (trainHub.isConnected()) {
-
-    char hubName[] = "trainHub";
-    trainHub.setHubName(hubName);
-
-
-    int speed = getSpeed(trainState);
-
-    trainHub.setBasicMotorSpeed(port, speed);
-    
-  } else {
-    Serial.println("Train hub is disconnected");
-    delay(1000);
+  String recievedData = "";
+  
+  if (Serial.available() <= 0) {
+    return;
   }
 
-} // End of loop
+  recievedData = Serial.readStringUntil('\n');
+  recievedData.trim();
+
+  if (recievedData.equals(STOP_COMMAND)) trainState = STOPPED;
+  if (recievedData.equals(SLOW_COMMAND)) trainState = SLOW;
+  if (recievedData.equals(FAST_COMMAND)) trainState = FAST;
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  unsigned long deltaT = currentMillis - previousMillis; // Time elapsed between last speed change and now.
+  setState();
+  handleInput();
+
+  if (deltaT < speedSwitchInterval) { // If we haven't reached the interval to change speed we should not change the speed.
+    return;
+  }
+
+  previousMillis = currentMillis;
+
+  if (!connect()) {
+    // Serial.println("Train hub is disconnected");
+    return;
+  }
+    
+  if (!trainHub.isConnected()) {
+    // Serial.println("Train hub is disconnected");
+    return;
+  }
+
+  Serial.write("Train state: ");
+  Serial.println((int)trainState, DEC);
+  
+  char hubName[] = "trainHub";
+  trainHub.setHubName(hubName);
+
+  int speed = getSpeed(trainState);
+
+  Serial.write("Speed: ");
+  Serial.println(speed, DEC);
+
+  trainHub.setBasicMotorSpeed(port, speed);
+}

aef8a72fddf247678e2d35afeb26355f4eb07eda
IwanIDev <iwan@iwani.dev>
2025-07-24 09:53:49 +0100

    Added functionality to the light sensor controller to check each sensor for train

--- include/LightSensorController.hpp
+++ include/LightSensorController.hpp
@@ -1,6 +1,17 @@
+#ifndef LIGHTSENSORCONTROLLER_HPP
+#define LIGHTSENSORCONTROLLER_HPP
+#define MAX_SENSORS 8
+#include "LightSensor.hpp"
+
 class LightSensorController {
 private:
-
+    LightSensor* sensors[MAX_SENSORS]; // Array of LightSensor pointers
+    int sensorCount;
 public:
+    LightSensorController();
 
+    bool isTrainPassingOver();
+
+    void addSensor(LightSensor* sensor);
 };
+#endif // LIGHTSENSORCONTROLLER_HPP

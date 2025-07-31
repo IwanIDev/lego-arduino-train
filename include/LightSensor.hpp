23ee3d5ce9180ee8834f8dce7f7da52f77c2ff9d
IwanIDev <iwan@iwani.dev>
2025-07-31 10:46:03 +0100

    Created sensor action class to define what a sensor should do

--- include/LightSensor.hpp
+++ include/LightSensor.hpp
@@ -2,6 +2,7 @@
 #define LIGHTSENSOR_HPP
 
 #define BUFFER_SIZE 64
+#include "SensorAction.hpp"
 
 class LightSensor {
 private:
@@ -15,19 +16,32 @@ private:
   int lightBufferIndex;
   bool bufferFull;
   int lastAverage;
+  SensorLocation location;
 
   int getAverageLightLevel();
   int getDynamicThreshold(int average);
 
 public:
-  LightSensor(int sensorPin, int detectionThreshold);
-  
+  LightSensor(int sensorPin, int detectionThreshold, SensorLocation loc)
+      : pin(sensorPin),
+        threshold(detectionThreshold),
+        lastReading(0),
+        trainDetected(false),
+        timeout(0),
+        timeoutThreshold(1000), // Set a default value or pass as parameter if needed
+        lightBuffer{0},
+        lightBufferIndex(0),
+        bufferFull(false),
+        lastAverage(0),
+        location(loc) {}
+
   int readLevel();
   bool isTrainPassingOver(int lightReading);
   bool detectPassingTrain();
-  
+
   bool isTrainDetected() const;
   void reset();
+  SensorLocation getLocation() const { return location; }
 };
 
 #endif // LIGHTSENSOR_HPP
